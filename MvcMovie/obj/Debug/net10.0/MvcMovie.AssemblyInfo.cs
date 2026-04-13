06954414ad2f62b632704aeb472ff5da98faa337
Davod Dice <david.c4f@hotmail.com>
2026-04-13 13:59:39 +0700

    update elte calendar

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42f61f7f5a5abbf7c5000f6941cae6465da83695")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+005d120a9a91d178cd50ae146757a9db0ccbaff9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
