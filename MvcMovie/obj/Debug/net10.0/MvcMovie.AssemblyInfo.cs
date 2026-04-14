f647f261c50c21f68917131658db42897b97c112
Davod Dice <david.c4f@hotmail.com>
2026-04-14 21:30:59 +0700

    remove unused code

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2502539bcb095b16bc97dbd6f63d393570cbf2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fd5c5536ac60453c1fc72e9cdb56b06d8a14530")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
