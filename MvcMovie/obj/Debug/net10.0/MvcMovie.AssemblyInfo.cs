302d8b5607fde7b0d56ad57ec0fe73e73b1c63f6
Davod Dice <david.c4f@hotmail.com>
2026-04-17 08:47:14 +0700

    add Fan visulization family-name

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f36d7926bd2ac64a423448d7afb4e26d744d9530")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dae7e2145b6056d6a31522eac1ab70e151b04079")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
